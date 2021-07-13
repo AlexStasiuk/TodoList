d33642a3283ae9056a46b8e57e7033513cabce55
AlexStasiuk <sov853972@gmail.com>
2021-07-13 11:35:06 +0300

    fix styles

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import AppHeader from '../app-header';
-import SearchPanel from '../search-panel/search-panel';
+import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
 
@@ -11,10 +11,8 @@ const App = () => {
 
   const todoData = [
     { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: false, id: 2 },
-    { label: 'Have a lunct', important: true, id: 3 },
-    { label: 'Have a lunct', important: false, id: 4 },
-    { label: 'Have a lunct', important: false, id: 5 }
+    { label: 'Make Awesome App', important: true, id: 2 },
+    { label: 'Have a lunch', important: false, id: 3 }
   ];
 
   return (
