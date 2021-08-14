0e2d99bb155d95bb9e899aab6bed54c8cb6cfec1
AlexStasiuk <sov853972@gmail.com>
2021-08-14 14:57:08 +0300

    refactor app.js

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,94 +8,101 @@ import ItemAddForm from '../item-add-form';
 import './app.css';
 
 export default class App extends React.Component{
-  constructor(){
-    super();
-    this.state = {
-      todoData : [
-        { label: 'Drink Coffee', important: false, done: false, id: 1 },
-        { label: 'Make Awesome App', important: true, done: true, id: 2 },
-        { label: 'Have a lunch', important: false, done: false, id: 3 }
-      ]
-    };
-    this.deleteItem = (id) =>{
-      this.setState((state) =>{
-        const idx = state.todoData.findIndex((el) => el.id === id);
-        console.log(idx);
-        
-        const before = state.todoData.slice(0,idx);
-        const after = state.todoData.slice(idx + 1);
-        return{
-          todoData: [...before, ...after]
-        }
-      });
-    };
-    this.toggleProperty = (arr, id, propName) => {
-      const idx = arr.findIndex((item) => item.id === id);
-      const oldItem = arr[idx];
-      const value = !oldItem[propName];
-  
-      const item = { ...arr[idx], [propName]: value } ;
-      console.log(item,"deleted");
-      return [
-        ...arr.slice(0, idx),
-        item,
-        ...arr.slice(idx + 1)
-      ];
-    };
-    this.onToggleImportant = (id) => {
-      this.setState((state) => {
-        const todoData = this.toggleProperty(state.todoData, id, 'important');
-        return { todoData };
-      });
-    };
-    this.onToggleDone = (id) => {
-      this.setState((state) => {
-        const todoData = this.toggleProperty(state.todoData, id, 'done');
-        return { todoData };
-      });
-    };
-    this.doneItems = () =>{
-      let res = 0;
-      const {todoData} = this.state;
-      todoData.forEach((elem)=>{
-        if(elem.done){
-          res++;
-        }
-      });
-      return res;
-    };
-    this.getMaxId = () =>{
+
+  maxId = 1;
+  state = {
+    todoData : [
+      this.createNewItem("Drink coffee"),
+      this.createNewItem("Make Awesome App"),
+      this.createNewItem("Have a lunc")
+    ]
+  }
+  createNewItem (label){
+    return{
+      label,
+      important: false,
+      done: false,
+      id: this.maxId++
+    }
+  }
+  getMaxId(){
+    let maxId = 0;
+    const {todoData} = this.state;
+    todoData.forEach((elem) => {
+      if(elem.id > maxId){
+        maxId = elem.id;
+      }
+    });
+    console.log(maxId);
+    return maxId;
+  }
+
+  deleteItem = (id) =>{
+    this.setState((state) =>{
+      const idx = state.todoData.findIndex((el) => el.id === id);
+      console.log(idx);
+      
+      const before = state.todoData.slice(0,idx);
+      const after = state.todoData.slice(idx + 1);
+      return{
+        todoData: [...before, ...after]
+      }
+    });
+  }
+  toggleProperty(arr, id, propName){
+    const idx = arr.findIndex((item) => item.id === id);
+    const oldItem = arr[idx];
+    const value = !oldItem[propName];
+
+    const item = { ...arr[idx], [propName]: value } ;
+    console.log(item,"deleted");
+    return [
+      ...arr.slice(0, idx),
+      item,
+      ...arr.slice(idx + 1)
+    ];
+  }
+  onToggleImportant = (id) => {
+    this.setState((state) => {
+      const todoData = this.toggleProperty(state.todoData, id, 'important');
+      return { todoData };
+    });
+  }
+  onToggleDone = (id) => {
+    this.setState((state) => {
+      const todoData = this.toggleProperty(state.todoData, id, 'done');
+      return { todoData };
+    });
+  }
+  doneItems(){
+    let res = 0;
+    const {todoData} = this.state;
+    todoData.forEach((elem)=>{
+      if(elem.done){
+        res++;
+      }
+    });
+    return res;
+  }
+
+  addNewItem = (text) => {
+    this.setState((state) =>{
       let maxId = 0;
-      const {todoData} = this.state;
-      todoData.forEach((elem) => {
+      
+      this.state.todoData.forEach((elem) => {
         if(elem.id > maxId){
           maxId = elem.id;
         }
       });
-      console.log(maxId);
-      return maxId;
-    };
-    this.addNewItem = (text) =>{
-      this.setState((state) =>{
-        let maxId = 0;
-        
-        this.state.todoData.forEach((elem) => {
-          if(elem.id > maxId){
-            maxId = elem.id;
-          }
-        });
-        const newId = maxId + 1;
-        
-        const newItem = {id: newId, label: text, important: true, done: false};
-        const todoData = [...state.todoData, newItem];
-        return{
-          todoData
-        };
-      });
-    };
-
+      const newId = maxId + 1;
+      
+      const newItem = {id: newId, label: text, important: false, done: false};
+      const todoData = [...state.todoData, newItem];
+      return{
+        todoData
+      };
+    });
   }
-
   render(){
     return (
       <div className="todo-app">
